544e651c3ea0adb8fcf8dedccce06b7c437f1cac
EC2 Default User <cooler970708@gmail.com>
2021-10-12 07:33:22 +0000

    編集機能

--- aiueo/sample_app/app/controllers/todolists_controller.rb
+++ aiueo/sample_app/app/controllers/todolists_controller.rb
@@ -19,6 +19,16 @@ def show
   @list = List.find(params[:id])
 end
 
+def edit
+  @list = List.find(params[:id])
+end
+
+def update
+  list = List.find(params[:id])
+  list.update(list_params)
+  redirect_to todolist_path(list.id)
+end
+
 private
 #ストロングパラメータ
 def list_params
